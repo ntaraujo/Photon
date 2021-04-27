b8fabcc7a9aad202e56e150eef84af2c04a30972
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-04-27 08:59:10 -0300

    define method names for numeric nodes

--- core/tree/numeric.py
+++ core/tree/numeric.py
@@ -6,21 +6,26 @@ from node import FuncLike
 class Complex(FuncLike):  # C
     __slots__ = ()
     methods = {Add, Sub, Mul, TrueDiv, Pow, Neg, Pos, Abs}
+    name = 'complex'
 
 
 class Float(Complex):  # R
     __slots__ = ()
     methods = {*Complex.methods, FloorDiv, Mod, DivMod}
+    name = 'float'
 
 
 class Exact(Float):  # Q
     __slots__ = ()
+    name = 'exact'
 
 
 class Integer(Float):  # Z
     __slots__ = ()
     methods = {*Exact.methods, LShift, RShift, And, Xor, Or, Invert}
+    name = 'int'
 
 
 class Natural(Integer):  # N
     __slots__ = ()
+    name = 'natural'
