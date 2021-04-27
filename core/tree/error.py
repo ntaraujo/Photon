bc7c12b39c7d9c99124cb54db3d5831f04a4964f
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-04-27 08:26:11 -0300

    remove useless InternalRaise

--- core/tree/error.py
+++ core/tree/error.py
@@ -5,18 +5,11 @@ line_number = 0
 
 class Raise:
     def __init__(self, node, error, msg):
-        import sys
-        sys.tracebacklimit = 0
-
         where = f'File "{file}", line {line_number}, node {node.id}:'
         index = " " * line.index(node.line_slice) + '^'
 
         msg = f'\n{where}\n\t{line}\n\t{index}\n\t{msg}\n'
-        raise error(msg)
 
-
-class InternalRaise:
-    def __init__(self, error, msg):
         import sys
-        sys.tracebacklimit = 1000
+        sys.tracebacklimit = 0
         raise error(msg)
