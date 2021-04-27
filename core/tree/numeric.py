84bb76a1c0aed58f8d40d9813751d360c93f7fc1
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-04-27 08:36:56 -0300

    order number types by number sets

--- core/tree/numeric.py
+++ core/tree/numeric.py
@@ -3,24 +3,24 @@ from methods import Add, Sub, Mul, TrueDiv, Pow, Neg, Pos, Abs, \
 from node import FuncLike
 
 
-class Complex(FuncLike):
+class Complex(FuncLike):  # C
     __slots__ = ()
     methods = {Add, Sub, Mul, TrueDiv, Pow, Neg, Pos, Abs}
 
 
-class Exact(Complex):
+class Float(Complex):  # R
     __slots__ = ()
     methods = {*Complex.methods, FloorDiv, Mod, DivMod}
 
 
-class Integer(Exact):
+class Exact(Float):  # Q
+    __slots__ = ()
+
+
+class Integer(Float):  # Z
     __slots__ = ()
     methods = {*Exact.methods, LShift, RShift, And, Xor, Or, Invert}
 
 
-class Float(Exact):
-    __slots__ = ()
-
-
-class Natural(Integer):
+class Natural(Integer):  # N
     __slots__ = ()
