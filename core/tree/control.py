8a59db800c206dacb87ac9f2ab929ebe16187586
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-04-29 06:00:54 -0300

    rename error.py -> control.py

--- core/tree/control.py
+++ core/tree/control.py
@@ -1,12 +1,14 @@
 file = 'None'
 line = 'None'
+line_slice = 'None'
 line_number = 0
+node_id = 0
 
 
 class Raise:
-    def __init__(self, node, error, msg):
-        where = f'File "{file}", line {line_number}, node {node.id}:'
-        index = " " * line.index(node.line_slice) + '^'
+    def __init__(self, error, msg):
+        where = f'File "{file}", line {line_number}, node {node_id}:'
+        index = " " * line.index(line_slice) + '^'
 
         msg = f'\n{where}\n\t{line}\n\t{index}\n\t{msg}\n'
 
